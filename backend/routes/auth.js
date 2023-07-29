9f7d7fd8f7efbb2eb71e1a6f1e77ab0409c278b8
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-30 03:06:06 +0500

    Add auth routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,9 @@
 import express from 'express';
+import { login, register } from '../controllers/authController.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello, this is auth endpoint.');
-});
-
-router.get('/register', (req, res) => {
-  res.send('Hello, this is auth register endpoint');
-});
-
-router.get('/login', (req, res) => {
-  res.send('Hello this is auth login endpoint');
-});
+router.post('/register', register);
+router.post('/login', login);
 
 export default router;
