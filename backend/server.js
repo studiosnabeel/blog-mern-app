2273bc71517d744fa22fde34de2a0a8ee43092aa
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-29 20:21:15 +0500

    Mongodb connected to server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectToDB from './db/db.js';
+import mongoose from 'mongoose';
 
+dotenv.config();
 const app = express();
 
-app.listen(8000, () => {
-  console.log('server running successfully on port 8000');
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected');
+});
+
+const ports = process.env.PORT;
+
+app.listen(ports, () => {
+  connectToDB();
+  console.log(`server running successfully on port: ${ports}`);
 });
