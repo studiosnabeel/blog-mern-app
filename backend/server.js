afe84f96ea8696527219610dd4e0a4657cb88681
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-30 01:39:17 +0500

    Add error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import connectToDB from './db/db.js';
 import mongoose from 'mongoose';
 import authRoute from './routes/auth.js';
 import userRoute from './routes/user.js';
@@ -9,14 +8,36 @@ import blogRoute from './routes/blog.js';
 dotenv.config();
 const app = express();
 
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB);
+    console.log('MONGO_DB connected successfully');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 mongoose.connection.on('disconnected', () => {
   console.log('MongoDB disconnected');
 });
 
 //Middlewares
+app.use(express.json());
+
 app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute);
-app.use('/api/blog', blogRoute);
+app.use('/api/blogs', blogRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || 'Something went wrong.';
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
 const ports = process.env.PORT;
 
