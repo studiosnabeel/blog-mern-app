a569625e9070886e16b90027fe3ba506c217ba84
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-31 23:10:21 +0500

    Create jsx and css for login page

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './login.css';
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="lContainer">
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="lInput"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="lInput"
+        />
+        <button className="lButton">Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
