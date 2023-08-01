bfb9b8a25f8f4009cc5ed4810b12cd12ffc4ad70
Nabeel Akbar <studiosnabeel@gmail.com>
2023-08-01 14:22:53 +0500

    Apply cors middleware in server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import authRoute from './routes/auth.js';
-import userRoute from './routes/user.js';
-import blogRoute from './routes/blog.js';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import userRoute from "./routes/user.js";
+import blogRoute from "./routes/blog.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -12,27 +13,28 @@ const app = express();
 const connectToDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_DB);
-    console.log('MONGO_DB connected successfully');
+    console.log("MONGO_DB connected successfully");
   } catch (err) {
     console.log(err);
   }
 };
 
-mongoose.connection.on('disconnected', () => {
-  console.log('MongoDB disconnected');
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected");
 });
 
 //Middlewares
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors());
 
-app.use('/api/auth', authRoute);
-app.use('/api/user', userRoute);
-app.use('/api/blogs', blogRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/blogs", blogRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
-  const errorMessage = err.message || 'Something went wrong.';
+  const errorMessage = err.message || "Something went wrong.";
   return res.status(errorStatus).json({
     success: false,
     status: errorStatus,
