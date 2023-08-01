8f71c90f726fef91f7262986686a521bdbe71f4d
Nabeel Akbar <studiosnabeel@gmail.com>
2023-08-02 01:59:43 +0500

    Add register functionality to frontend

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
-import './register.css';
+import React, { useContext, useState } from "react";
+import "./register.css";
+import { authContext } from "../../context/authContext";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    email: undefined,
+    password: undefined,
+  });
+
+  const { loading, error, dispatch } = useContext(authContext);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post(
+        "http://localhost:8000/api/auth/register",
+        credentials
+      );
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+      navigate("/login");
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
+    }
+  };
+
   return (
     <div className="login">
       <div className="lContainer">
@@ -9,16 +41,27 @@ const Register = () => {
           type="text"
           placeholder="username"
           id="username"
+          onChange={handleChange}
           className="lInput"
         />
-        <input type="email" placeholder="email" id="email" className="lInput" />
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          onChange={handleChange}
+          className="lInput"
+        />
         <input
           type="password"
           placeholder="password"
           id="password"
+          onChange={handleChange}
           className="lInput"
         />
-        <button className="lButton">Register</button>
+        <button disabled={loading} onClick={handleClick} className="lButton">
+          Register
+        </button>
+        {error && <span>{error.message}</span>}
       </div>
     </div>
   );
